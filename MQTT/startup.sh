8df486c64622692f6c9fa813ae2455446175d710
aditisa1 <aditi.saxena@intel.com>
2021-12-03 12:01:59 +0530

    UWC : Modified the tags of the containers
    Earlier containers were referring to EII_VERSION tag modified it to
    UWC_VERSION.
    
    Signed-off-by: aditisa1 <aditi.saxena@intel.com>

---
+++ MQTT/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#chown -R $EIIUID:$EIIUID /mosquitto/log
+chown -R $EIIUID:$EIIUID /mosquitto/config /mosquitto/data /run/secrets
+#chmod -R 760 /tmp
+chmod -R 755 /run/secrets
+
+exec runuser -u $EIIUSER -- $@
